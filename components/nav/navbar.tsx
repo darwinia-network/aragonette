93a560b785e5da5d02683324b8f2bd0edaafb4d6
Hamid Roohi <hamidroohi71@gmail.com>
2024-10-04 18:41:27 +0330

    fix(text and title): fix logo and text  changes by network changes

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -7,9 +7,13 @@ import Link from "next/link";
 import { useState } from "react";
 import { MobileNavDialog } from "./mobileNavDialog";
 import { NavLink, type INavLink } from "./navLink";
+import { useChainId } from "wagmi";
 
 export const Navbar: React.FC = () => {
   const [open, setOpen] = useState(false);
+  const chainId = useChainId();
+
+  console.log("chain id", chainId);
 
   const navLinks: INavLink[] = [
     // { path: "/", id: "dashboard", name: "Dashboard", icon: IconType.APP_DASHBOARD },
@@ -23,21 +27,24 @@ export const Navbar: React.FC = () => {
 
   return (
     <>
-      <nav className="h-30 top-0 w-full flex-col gap-2 border-b border-b-neutral-100 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
+      <nav className="h-30 top-0 z-10 w-full flex-col gap-2 border-b border-b-neutral-100 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
         <div className="flex w-full items-center justify-between">
           <Link
             href="/"
             className={classNames(
-              "flex items-center gap-x-3 rounded-full md:rounded-lg",
+              "flex cursor-pointer items-center gap-x-3 rounded-full md:rounded-lg",
               "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
             )}
           >
-            <Image src="/images/logo-icon.png" width="36" height="36" className="shrink-0" alt="Ring Dao" />
+            {(chainId === 46 || chainId === 701) && (
+              <Image src="/images/logo-icon.png" width="36" height="36" className="shrink-0" alt="Ring Dao" />
+            )}
             <Image src="/images/logo.png" width="100" height="36" className="shrink-0" alt="Ring Dao" />
-
-            {/* <span className="hidden py-1 text-lg font-semibold leading-tight text-[#fff] sm:block md:text-xl">
-              Aragonette
-            </span> */}
+            {chainId === 701 && (
+              <span className="hidden py-1 text-lg font-semibold leading-tight text-[#fff] sm:block md:text-xl">
+                Test Version
+              </span>
+            )}
           </Link>
           <div className="z-20 flex items-center gap-x-2">
             <div className="shrink-0">
